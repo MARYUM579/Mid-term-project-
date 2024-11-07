ec29b4262045af7f63d77d3264d2cbadc7f32fda
MARYUM <arshemanmemon@gmail.com>
2024-11-07 05:05:04 -0800

    Add files via upload

---
+++ frist-2.py
@@ -0,0 +1,43 @@
+
+num1=int(input("Enter value 1:"))
+num2=int(input("Enter value 2:"))
+opr=input("Enter the opr...(+,-,*,/,%)")
+
+if opr=="+":
+   print(num1+num2)
+if opr=="-":
+   print(num1-num2)
+if opr=="*":
+  print(num1*num2)
+if opr=="/":
+  print(num1/num2)
+if opr=="%":
+  print(num1%num2)
+
+else:
+  print("invalid opr...")
+
+
+
+
+
+
+num1=float(input("enter frist number:"))
+op=(input("Enter operator:"))
+num2=float(input("Enter second number:"))
+if op=="+":
+  print(num1+num2)
+if opr=="-":
+   print(num1-num2)
+if opr=="*":
+  print(num1*num2)
+if opr=="/":
+  print(num1/num2)
+if opr=="%":
+  print(num1%num2)
+
+
+  calc=input("type calculation:/n")
+  print("Answer:" +str(eval(calc)))
+
+  
